6c0416bf17eaedafbc81ae3554db07ca72fe2dd2
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-09-06 16:40:32 +0200

    fix template index type

--- resources/views/admin/type/index.blade.php
+++ resources/views/admin/type/index.blade.php
@@ -30,7 +30,7 @@
                         <td>{{ $type->created_at }}</td>
                         <td>{{ $type->updated_at }}</td>
                         <td>
-                            <div class="d-flex">
+                            <div class="d-flex justify-content-end">
 
                                 <a href="{{ route('admin.types.show', $type) }}" class="btn btn-primary me-2">
                                     <i class="fa-solid fa-eye"></i>
@@ -48,8 +48,8 @@
                     </tr>
                 @empty
                     <tr>
-                        <td class="text-center" colspan="5">
-                            <h3>Non ci sono progetti</h3>
+                        <td class="text-center" colspan="6">
+                            <h3>Non ci sono categorie</h3>
                         </td>
                     </tr>
                 @endforelse
@@ -59,7 +59,7 @@
         <div class="d-flex align-items-center justify-content-between">
             <a href="{{ route('admin.projects.create') }}" class="btn btn-primary">
                 <i class="fa-solid fa-plus"></i>
-                Aggiungi Nuovo Progetto
+                Aggiungi Nuova categoria
             </a>
             <a href="{{ route('admin.projects.trash') }}" class="btn btn-secondary">
                 <i class="fa-solid fa-trash"></i>
