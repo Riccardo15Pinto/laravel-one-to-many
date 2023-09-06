499f130ec932838f4f835740fbc0d4fa34a7fb99
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-09-06 16:37:55 +0200

    set show page

---
+++ resources/views/admin/type/show.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app')
+@section('title', 'Admin/Type/detail')
+
+@section('content')
+    <div class="container">
+        <h2 class="fs-4 text-secondary my-4">
+            Dettaglio tipo:
+        </h2>
+
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <div class="d-flex align-items-center">
+
+                    <div>
+
+                        <h5 class="card-title">{{ $type->label }}</h5>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">Color : <span class="badge"
+                                style="background-color: {{ $type->color }}">{{ $type->color }}</span></h6>
+
+                    </div>
+                    <div>
+                        <a href="#" class="btn btn-warning">
+                            <i class="fa-solid fa-pen-nib"></i>
+                            Edit
+                        </a>
+                        <a href="#" class="btn btn-danger">
+                            <i class="fa-solid fa-trash-can"></i>
+                            Delete
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+@endsection
+
+@section('scripts')
+
+    @vite('resources/js/toast.js')
+@endsection
